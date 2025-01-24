fdf3031fc7d86408eea3a37db00d63c6d5091639
Mou Hongjian (IT SBI BIA CCA) <HongJian.Mou@infineon.com>
2025-01-24 23:10:30 +0800

    Make SANDBOX_USER_UID configurable via environment variable

--- internal/static/user.go
+++ internal/static/user.go
@@ -1,6 +1,19 @@
 package static
 
+import (
+	"os"
+	"strconv"
+)
+
 const SANDBOX_USER = "sandbox"
-const SANDBOX_USER_UID = 2000651600
+
+var SANDBOX_USER_UID = func() int {
+	uidStr := os.Getenv("SANDBOX_USER_UID")
+	uid, err := strconv.Atoi(uidStr)
+	if err != nil {
+		return 2000651600 // default value if environment variable is not set
+	}
+	return uid
+}()
 
 var SANDBOX_GROUP_ID = 0
